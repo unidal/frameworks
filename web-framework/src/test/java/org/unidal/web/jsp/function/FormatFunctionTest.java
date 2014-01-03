7dc36df0b63cab53a8f5756b57191c1f87a744bc
Frankie Wu <qmwu2000@gmail.com>
2014-01-03 16:37:18 +0800

    test case fix

--- web-framework/src/test/java/org/unidal/web/jsp/function/FormatFunctionTest.java
+++ web-framework/src/test/java/org/unidal/web/jsp/function/FormatFunctionTest.java
@@ -7,21 +7,22 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 public class FormatFunctionTest {
-	@Test
-	public void testFormat() {
-		Assert.assertEquals("12345", FormatFunction.format(12345, "0.#"));
-		Assert.assertEquals("12345.0", FormatFunction.format(12345, "0.0"));
-		Assert.assertEquals("12,345.0", FormatFunction.format(12345, "#,##0.0"));
-		Assert.assertEquals("2013", FormatFunction.format(new Date(), "yyyy"));
-	}
+   @SuppressWarnings("deprecation")
+   @Test
+   public void testFormat() {
+      Assert.assertEquals("12345", FormatFunction.format(12345, "0.#"));
+      Assert.assertEquals("12345.0", FormatFunction.format(12345, "0.0"));
+      Assert.assertEquals("12,345.0", FormatFunction.format(12345, "#,##0.0"));
+      Assert.assertEquals(String.valueOf(new Date().getYear() + 1900), FormatFunction.format(new Date(), "yyyy"));
+   }
 
-	@Test
-	public void testFormatNumber() {
-		Assert.assertEquals("12.1KB", FormatFunction.formatNumber(12345, "0.#", "B"));
-		Assert.assertEquals("12.06Kb", FormatFunction.formatNumber(12345, "0.0#", "b"));
+   @Test
+   public void testFormatNumber() {
+      Assert.assertEquals("12.1KB", FormatFunction.formatNumber(12345, "0.#", "B"));
+      Assert.assertEquals("12.06Kb", FormatFunction.formatNumber(12345, "0.0#", "b"));
 
-		Assert.assertEquals("12.34s", FormatFunction.formatNumber(12.345, "0.##", "s"));
-		Assert.assertEquals("123.4ms", FormatFunction.formatNumber(0.12345, "0.#", "s"));
-		Assert.assertEquals("123.4us", FormatFunction.formatNumber(0.00012345, "0.#", "s"));
-	}
+      Assert.assertEquals("12.34s", FormatFunction.formatNumber(12.345, "0.##", "s"));
+      Assert.assertEquals("123.4ms", FormatFunction.formatNumber(0.12345, "0.#", "s"));
+      Assert.assertEquals("123.4us", FormatFunction.formatNumber(0.00012345, "0.#", "s"));
+   }
 }
