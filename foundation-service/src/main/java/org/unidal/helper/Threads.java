4152e9a0c8910cbe04bc6b2646dc53c62a89d458
Frankie Wu <qmwu2000@gmail.com>
2016-05-07 16:59:55 +0800

    fix logging issue

--- foundation-service/src/main/java/org/unidal/helper/Threads.java
+++ foundation-service/src/main/java/org/unidal/helper/Threads.java
@@ -142,7 +142,7 @@ public class Threads {
 
       @Override
       public boolean onUncaughtException(Thread thread, Throwable e) {
-         m_logger.error(String.format("Uncaught exception thrown out of thread(%s).", thread.getName()), e);
+         m_logger.error(String.format("Uncaught exception thrown out of thread(%s)!", thread.getName()), e);
          return true;
       }
 
@@ -297,8 +297,8 @@ public class Threads {
 
       public void shutdown() {
          if (m_target instanceof Task) {
-            System.out.println("shutdown task " + ((Task) m_target).getName());
             ((Task) m_target).shutdown();
+            System.out.println(String.format("Task(%s) is shutdown! ", getName()));
          } else {
             interrupt();
          }
@@ -318,11 +318,14 @@ public class Threads {
 
       private boolean m_active;
 
+      private boolean m_deamon;
+
       public ThreadGroupManager(UncaughtExceptionHandler handler, String name) {
          m_threadGroup = new ThreadGroup(name);
          m_factory = new DefaultThreadFactory(m_threadGroup);
          m_factory.setUncaughtExceptionHandler(handler);
          m_active = true;
+         m_deamon = true;
       }
 
       public boolean isActive() {
@@ -364,6 +367,7 @@ public class Threads {
       }
 
       public ThreadGroupManager nonDaemon() {
+         m_deamon = false;
          return this;
       }
 
@@ -386,7 +390,7 @@ public class Threads {
       }
 
       public Thread start(Runnable runnable) {
-         return start(runnable, true);
+         return start(runnable, m_deamon);
       }
 
       public Thread start(Runnable runnable, boolean deamon) {
