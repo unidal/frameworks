1894e797720a96ada75c9cca7dbd4dea0b139c30
Frankie Wu <qmwu2000@gmail.com>
2017-09-20 15:25:16 +0800

    fix ContainerLoader to avoid multiple container instances

--- foundation-service/src/main/java/org/unidal/lookup/ContainerLoader.java
+++ foundation-service/src/main/java/org/unidal/lookup/ContainerLoader.java
@@ -6,82 +6,86 @@ import org.codehaus.plexus.PlexusContainer;
 import org.unidal.lookup.container.MyPlexusContainer;
 
 public class ContainerLoader {
-   private static volatile PlexusContainer s_container;
+	private static volatile PlexusContainer s_container;
 
-   public static void destroy() {
-      if (s_container != null) {
-         s_container.dispose();
-         s_container = null;
-      }
-   }
+	public static void destroy() {
+		if (s_container != null) {
+			s_container.dispose();
+			s_container = null;
+		}
+	}
 
-   public static PlexusContainer getDefaultContainer() {
-      return getDefaultContainer(null);
-   }
+	public static PlexusContainer getDefaultContainer() {
+		return getDefaultContainer(null);
+	}
 
-   public static PlexusContainer getDefaultContainer(String configuration) {
-      if (s_container == null) {
-         try {
-            if (configuration != null) {
-               InputStream in = ContainerLoader.class.getClassLoader().getResourceAsStream(configuration);
+	public static PlexusContainer getDefaultContainer(String configuration) {
+		if (s_container == null) {
+			synchronized (ContainerLoader.class) {
+				if (s_container == null) {
+					try {
+						if (configuration != null) {
+							InputStream in = ContainerLoader.class.getClassLoader().getResourceAsStream(configuration);
 
-               s_container = new MyPlexusContainer(in);
-            } else {
-               s_container = new MyPlexusContainer();
-            }
-         } catch (Exception e) {
-            throw new RuntimeException("Unable to create Plexus container!", e);
-         }
-      }
+							s_container = new MyPlexusContainer(in);
+						} else {
+							s_container = new MyPlexusContainer();
+						}
+					} catch (Exception e) {
+						throw new RuntimeException("Unable to create Plexus container!", e);
+					}
+				}
+			}
+		}
 
-      return s_container;
-   }
+		return s_container;
+	}
 
-   static class Key {
-      private Class<?> m_role;
+	static class Key {
+		private Class<?> m_role;
 
-      private String m_roleHint;
+		private String m_roleHint;
 
-      private String m_id;
+		private String m_id;
 
-      public Key(Class<?> role, String roleHint, String id) {
-         m_role = role;
-         m_roleHint = roleHint == null ? "default" : roleHint;
-         m_id = id;
-      }
+		public Key(Class<?> role, String roleHint, String id) {
+			m_role = role;
+			m_roleHint = roleHint == null ? "default" : roleHint;
+			m_id = id;
+		}
 
-      @Override
-      public boolean equals(Object obj) {
-         if (obj instanceof Key) {
-            Key e = (Key) obj;
+		@Override
+		public boolean equals(Object obj) {
+			if (obj instanceof Key) {
+				Key e = (Key) obj;
 
-            if (e.m_role != m_role) {
-               return false;
-            }
+				if (e.m_role != m_role) {
+					return false;
+				}
 
-            if (!e.m_roleHint.equals(m_roleHint)) {
-               return false;
-            }
+				if (!e.m_roleHint.equals(m_roleHint)) {
+					return false;
+				}
 
-            if (!e.m_id.equals(m_id)) {
-               return false;
-            }
+				if (!e.m_id.equals(m_id)) {
+					return false;
+				}
 
-            return true;
-         }
+				return true;
+			}
 
-         return false;
-      }
+			return false;
+		}
 
-      @Override
-      public int hashCode() {
-         int hashCode = 0;
+		@Override
+		public int hashCode() {
+			int hashCode = 0;
 
-         hashCode = hashCode * 31 + m_role.hashCode();
-         hashCode = hashCode * 31 + m_roleHint.hashCode();
-         hashCode = hashCode * 31 + m_id.hashCode();
+			hashCode = hashCode * 31 + m_role.hashCode();
+			hashCode = hashCode * 31 + m_roleHint.hashCode();
+			hashCode = hashCode * 31 + m_id.hashCode();
 
-         return hashCode;
-      }
-   }
+			return hashCode;
+		}
+	}
 }
